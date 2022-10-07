4c2baf112e62c4d81311958109298b79e55e8e60
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2022-10-07 10:53:48 -0300

    Updated theme

---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.tsx", "./index.html"],
+  theme: {
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+    },
+    extend: {
+      backgroundImage: {
+        galaxy: "url('/background-galaxy.png')",
+        "nlw-gradient":
+          "linear-gradient(89.86deg, #9572FC 30.08%, #43E7AD 43.94%, #E1D55D 87.57%),linear-gradient(0deg, #FFFFFF, #FFFFFF)",
+        "game-gradient":
+          "linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)",
+      },
+    },
+  },
+  plugins: [],
+};
